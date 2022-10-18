82667d87949ae942b1e8bca635120a3161de219c
“daliashaman” <daliashaman@gmail.com>
2022-10-17 21:16:10 -0400

    longest-name

--- longest-name/script.js
+++ longest-name/script.js
@@ -4,12 +4,12 @@ let name1 = "Brandon";
 let name2 = "Dalia";
 let name3 ="Idon";
 
-if(name1 > name2){
-    console.log('Brandon has the longest name!');
+if(name3.length > name2.length && name3.length > name1.length){
+    console.log(`${name3} has the longest name!`);
 }
-else if(name2 > name3){
-    console.log('Dalia has the longest name!');
+else if(name2.length > name1.length && name2.length > name3.length){
+    console.log(`${name2} has the longest name!`); 
 }
 else{
- console.log('Brandon has the longest name of all three names!');   
+ console.log(`${name1} has the longest name of all three names!`);   
 }
