03e1b4807e2b84f7c2739548c7732f7de28716ba
“daliashaman” <daliashaman@gmail.com>
2022-10-17 21:52:34 -0400

    Counting Loops

---
+++ counting-loops/loops.js
@@ -0,0 +1,70 @@
+// FOR LOOPS
+
+//log numbers from 1 to 10, then counting down from 10 to 1
+
+// for(let i = 1; i <= 10; i++){
+//     console.log(i);
+// }
+
+// for(let i = 10; i >= 1; i--){
+//     console.log(i);
+// }
+
+//--------------------------------
+//WHILE LOOP
+
+// let i = 1;
+// while(i <= 10){
+//     console.log(i);
+//     i++;
+// }
+
+// let i = 1;
+// while(i <= 10){
+//     console.log(i);
+//     i++;
+// }
+
+// let i = 10;
+// while(i >= 1){
+//     console.log(i);
+//     i--;
+// }
+
+//--------------------------------
+//DO WHILE LOOP
+
+// let i = 1
+// do{    
+//     console.log(i);
+//     i++;
+// }
+// while(i <= 10);
+
+// let i = 10
+// do{    
+//     console.log(i);
+//     i--;
+// }
+// while(i >= 1);
+
+
+//--------------------------------
+//ARRAY
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9 ,10]
+// for (number of numbers){
+//     console.log(number);
+// }
+
+
+
+//--------------------------------
+//EXTENDED CHALLENGE
+
+//any string - use loop to add padding (spaces) to front to make string 10 characters long
+
+let words = ["      ball", " notebook", "     mouse"]
+for (word of words){
+    console.log(word);
+}
